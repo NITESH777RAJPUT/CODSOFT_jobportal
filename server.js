0955850d8310e30fda51dbe2727cf95a61bc97f8
NITESH777RAJPUT <nitesh02006@gmail.com>
2025-10-01 23:02:12 +0530

    hihi

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ connectDB(process.env.MONGO_URI);
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://codsoftjobportal.vercel.app", credentials: true }));
 
 // static uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
